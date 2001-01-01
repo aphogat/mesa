dc7d6fbe9c40ade6e7440652ed82e5e8495d2ca0
Anuj Phogat <anuj.phogat@gmail.com>
2000-12-31 16:36:20 -0800

    i965/gen9: Disable Mip Tail for the surface with tr_mode != TRMODE_NONE
    This fixed the buffer corruption happening in surfaces with tr_mode
    YF and YS. BSpec recommends disabling mip tails for non-mip-mapped
    surfaces. But, with this enabled I couldn't get correct data out of
    mip-mapped surface.
    
    Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>

--- src/mesa/drivers/dri/i965/gen8_surface_state.c
+++ src/mesa/drivers/dri/i965/gen8_surface_state.c
@@ -252,8 +252,11 @@ gen8_update_texture_surface(struct gl_context *ctx,
                        GEN7_SURFACE_MIN_LOD) |
              (intelObj->_MaxLevel - tObj->BaseLevel); /* mip count */
 
-   if (brw->gen >= 9)
+   if (brw->gen >= 9) {
       surf[5] |= SET_FIELD(tr_mode, GEN9_SURFACE_TRMODE);
+      /* Disable Mip Tail by setting a large value. */
+      surf[5] |= SET_FIELD(15, GEN9_SURFACE_MIP_TAIL_START_LOD);
+   }
 
    if (aux_mt) {
       surf[6] = SET_FIELD(mt->qpitch / 4, GEN8_SURFACE_AUX_QPITCH) |
@@ -432,8 +435,11 @@ gen8_update_renderbuffer_surface(struct brw_context *brw,
 
    surf[5] = irb->mt_level - irb->mt->first_level;
 
-   if (brw->gen >= 9)
+   if (brw->gen >= 9) {
       surf[5] |= SET_FIELD(tr_mode, GEN9_SURFACE_TRMODE);
+      /* Disable Mip Tail by setting a large value. */
+      surf[5] |= SET_FIELD(15, GEN9_SURFACE_MIP_TAIL_START_LOD);
+   }
 
    if (aux_mt) {
       surf[6] = SET_FIELD(mt->qpitch / 4, GEN8_SURFACE_AUX_QPITCH) |
