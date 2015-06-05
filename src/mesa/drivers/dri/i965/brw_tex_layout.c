9773b86a40aaa30b2ca7374fe158805bcad5112a
Anuj Phogat <anuj.phogat@gmail.com>
2015-06-05 10:41:24 -0700

    i965: Make a helper function intel_miptree_release_levels()
    Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>
    Cc: Ben Widawsky <ben@bwidawsk.net>

--- src/mesa/drivers/dri/i965/brw_tex_layout.c
+++ src/mesa/drivers/dri/i965/brw_tex_layout.c
@@ -805,6 +805,17 @@ intel_miptree_set_alignment(struct brw_context *brw,
    }
 }
 
+static void
+intel_miptree_release_levels(struct intel_mipmap_tree *mt)
+{
+   unsigned int level = 0;
+
+   for (level = mt->first_level; level <= mt->last_level; level++) {
+      free(mt->level[level].slice);
+      mt->level[level].slice = NULL;
+   }
+}
+
 void
 brw_miptree_layout(struct brw_context *brw,
                    struct intel_mipmap_tree *mt,
@@ -870,7 +881,6 @@ brw_miptree_layout(struct brw_context *brw,
          mt->tiling = brw_miptree_choose_tiling(brw, requested, mt);
 
       if (is_tr_mode_yf_ys_allowed) {
-         unsigned int level = 0;
          assert(brw->gen >= 9);
 
          if (mt->tiling == I915_TILING_Y ||
@@ -887,10 +897,7 @@ brw_miptree_layout(struct brw_context *brw,
          /* Failed to use selected tr_mode. Free up the memory allocated
           * for miptree levels in intel_miptree_total_width_height().
           */
-         for (level = mt->first_level; level <= mt->last_level; level++) {
-            free(mt->level[level].slice);
-            mt->level[level].slice = NULL;
-         }
+         intel_miptree_release_levels(mt);
       }
       i++;
    }
