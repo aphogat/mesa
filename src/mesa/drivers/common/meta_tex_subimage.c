1549bbdb36c9009a093ecb64f6918be2f32672de
Anuj Phogat <anuj.phogat@gmail.com>
2015-06-12 14:42:57 -0700

    mesa: Change the signature of _mesa_need_rgb_to_luminance_conversion()
    This allows us to handle cases when texImage->_BaseFormat doesn't match
    _mesa_format_get_base_format(texImage->Format). _BaseFormat is what we
    care about in this function.
    
    Signed-off-by: Anuj Phogat <anuj.phogat@gmail.com>

--- src/mesa/drivers/common/meta_tex_subimage.c
+++ src/mesa/drivers/common/meta_tex_subimage.c
@@ -262,6 +262,7 @@ _mesa_meta_pbo_GetTexSubImage(struct gl_context *ctx, GLuint dims,
    int full_height, image_height;
    struct gl_texture_image *pbo_tex_image;
    struct gl_renderbuffer *rb = NULL;
+   GLenum dstBaseFormat = _mesa_unpack_format_to_base_format(format);
    GLenum status, src_base_format;
    bool success = false, clear_channels_to_zero = false;
    float save_clear_color[4];
@@ -284,7 +285,8 @@ _mesa_meta_pbo_GetTexSubImage(struct gl_context *ctx, GLuint dims,
                                             type, GL_FALSE))
          return false;
 
-      if (_mesa_need_rgb_to_luminance_conversion(rb->Format, format))
+      if (_mesa_need_rgb_to_luminance_conversion(rb->_BaseFormat,
+                                                 dstBaseFormat))
          return false;
 
       if (_mesa_need_signed_unsigned_int_conversion(rb->Format, format, type))
